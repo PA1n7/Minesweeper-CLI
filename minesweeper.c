50f0945e693bdf853f85963205f6e5010ac5a0da
PA1n7 <sebasmerchanbla@gmail.com>
2024-08-06 14:12:25 -0500

    took a bit

--- minesweeper.c
+++ minesweeper.c
@@ -1,34 +1,30 @@
 #include <ncurses.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void printing();
 void moving_and_sleeping();
 void colouring();
 
+#define LAND 10
+#define OFF 11
+#define MINE 12
+
 int pos[2] = {0, 0};
 chtype currPos = '#';
 int size = 20;
-int bombs = 99;
+int bombs = 8;
 int q;
 int width;
 int height;
 int board[100][100];
+int found = 0;
+bool revealed = false;
 
 void save(){
     move(pos[0], pos[1]);
     currPos = mvinch(pos[0], pos[1]);
-}
-
-void mark(){
-    chtype cc = '!';
-    chtype empty = '#';
-    chtype back = mvinch(pos[0], pos[1]);
-    if (back == cc){
-        addch(empty);
-    }else if (back == empty){
-        addch(cc);
-    }
-    save();
 }
 
 void reset_pos(){
@@ -73,27 +69,139 @@ int check_around(int pos[2], int brd[height][width]){
     return count;
 }
 
-void reveal(int pos[2]){
-    int ll[height][width];
-     
+void reveal_all(){
+    revealed = true;
+    for(int y = 0; y<height; y++){
+        for (int x = 0; x<width; x++){
+            move(y, x);
+            if (board[y][x] == 0){
+                if (has_colors()) attron(COLOR_PAIR(OFF));
+                addstr(" ");
+                if(has_colors()) attroff(COLOR_PAIR(OFF));
+            }else if(board[y][x] == -1){
+                if (has_colors()) attron(COLOR_PAIR(MINE));
+                addstr(" ");
+                if(has_colors()) attroff(COLOR_PAIR(MINE));            
+            }else{
+                if (has_colors()) attron(COLOR_PAIR(LAND));
+                addstr(" ");
+                if(has_colors()) attroff(COLOR_PAIR(LAND));
+            }
+        }
+    }
+    save();
+}
+void won(){
+    move(height, 0);
+    addstr("__   _____  _   _  __        _____  _   _ _ \n");
+    addstr("\\ \\ / / _ \\| | | | \\ \\      / / _ \\| \\ | | |\n");
+    addstr(" \\ V / | | | | | |  \\ \\ /\\ / / | | |  \\| | |\n");
+    addstr("  | || |_| | |_| |   \\ V  V /| |_| | |\\  |_|\n");
+    addstr("  |_| \\___/ \\___/     \\_/\\_/  \\___/|_| \\_(_)\n");
+    addstr("Press q or escape to quit...");
+    refresh();
+}
+void mark(){
+    chtype cc = '!';
+    chtype empty = '#';
+    chtype back = mvinch(pos[0], pos[1]);
+    if (back == cc){
+        if (board[pos[0]][pos[1]] == -1) found--;
+        addch(empty);
+    }else if (back == empty){
+        if (board[pos[0]][pos[1]] == -1) found++;
+        addch(cc);
+    }
+    if (found == bombs){
+        reveal_all();
+        won();
+    }
+    save();
+}
+
+void init_colors(){
+    if (has_colors() == false){
+        addstr("Playing colorblind mode (no colors)\n");
+    }else{
+        start_color();
+        for (int i = 1; i<9; i++){
+            if(i<3){init_pair(i, COLOR_BLACK, COLOR_GREEN); continue;}
+            if(i<5){init_pair(i, COLOR_BLACK, COLOR_YELLOW); continue;}
+            if(i<9)init_pair(i, COLOR_WHITE, COLOR_RED);
+        } 
+        init_pair(LAND, COLOR_BLACK, COLOR_GREEN);
+        init_pair(OFF, COLOR_BLACK, COLOR_BLUE);
+        init_pair(MINE, COLOR_BLACK, COLOR_RED);
+    }
+}
+
+void put_num(int num){
+    char str[2];
+    sprintf(str, "%d", num);
+    if (has_colors()) attron(COLOR_PAIR(num));
+    addstr(str);
+    if (has_colors()) attroff(COLOR_PAIR(num));
+}
+
+
+bool zero_fix(){
+    chtype empty =  '#';
+    chtype zero = ' ';
+    bool added_zeroes = false;
+    for(int y = 0; y<height; y++){
+        for(int x = 0; x<width; x++){
+            if (mvinch(y, x) == zero){
+                int conf_changes[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+                for(int i = 0; i<4; i++){
+                    int new_pos[2] = {y+conf_changes[i][0], x+conf_changes[i][1]};
+                    if(mvinch(new_pos[0], new_pos[1]) != empty) continue;
+                    added_zeroes = true;
+                    if (check_aval(new_pos)){
+                        move(new_pos[0], new_pos[1]);
+                        int to_wr = board[new_pos[0]][new_pos[1]];
+                        if (to_wr == 0){
+                            addstr(" ");
+                        }else{
+                            put_num(to_wr);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return added_zeroes;
+}
+
+void lost(){
+    reveal_all();
+    move(height, 0);
+    addstr("__   _____  _   _   _     ___  ____ _____      __\n");
+    addstr("\\ \\ / / _ \\| | | | | |   / _ \\/ ___|_   _|  _ / /\n");
+    addstr(" \\ V / | | | | | | | |  | | | \\___ \\ | |   (_) | \n");
+    addstr("  | || |_| | |_| | | |__| |_| |___) || |    _| | \n");
+    addstr("  |_| \\___/ \\___/  |_____\\___/|____/ |_|   (_) | \n");
+    addstr("                                              \\_\\\n");
+    addstr("Press q or escape to quit...");
+    refresh();
 }
 
 void check(){
-    // Initializing empty array, doesn't matter
-    int ll[height][width];
-    int count = check_around(pos, ll);
-    char str[2];
-    sprintf(str, "%d", count);
-    move(pos[0], pos[1]);
-    if (count == -1){
-        addstr("w");
-    }else if (count == 0){
-        reveal(pos);
+    if (revealed != true){
+        int count = board[pos[0]][pos[1]];
+        char str[2];
+        sprintf(str, "%d", count);
+        move(pos[0], pos[1]);
+        if (count == -1){
+            lost();
+        }else if (count == 0){
+            addstr(" ");
+            while(zero_fix()) continue;
+        }
+        else{
+            put_num(count);
+        }
+        save();
     }
-    else{
-        addstr(str);
-    }
-    save();
 }
 
 void clear_board(int brd[height][width]){
@@ -143,6 +251,14 @@ void start_screen(){
     addstr("| |\\/| || ||  \\| |  _| \\___ \\\\ \\ /\\ / /|  _| |  _| | |_) |  _| | |_) | \\!!/\n");
     addstr("| |  | || || |\\  | |___ ___) |\\ V  V / | |___| |___|  __/| |___|  _ <  /__\\\n");
     addstr("|_|  |_|___|_| \\_|_____|____/  \\_/\\_/  |_____|_____|_|   |_____|_| \\_\\\n");
+    init_colors();
+    addstr("=====================================================\n");
+    addstr("Instructions:\n");
+    addstr("Use the W, A, S, D keys to move.\n");
+    addstr("To mark a bomb press the Space key.\n");
+    addstr("To reveal a square use the Enter key.\n");
+    addstr("Use Q or Esc to quit.\n");
+    addstr("=====================================================\n");
     addstr("Press any key to start...");
     refresh();
     cbreak();
@@ -159,6 +275,7 @@ void init_board(){
 }
 
 int main(){
+    srand(time(0));
     width = size*2.5;
     height = size;
     int board[height][width];
@@ -166,8 +283,7 @@ int main(){
     initscr();
     start_screen();
     refresh();
-        for (int y = 0; y<height; y++)
-    {
+    for (int y = 0; y<height; y++){
         for (int x = 0;x<width;x++)
         {
             addstr("#");
@@ -176,7 +292,19 @@ int main(){
         addstr("\n");
         refresh();
     }
-    board[0][0] = -1;
+    for (int i = 0; i<bombs; i++){
+        int posY = rand()%height;
+        int posX = rand()%width;
+        while (board[posY][posX] == -1){
+            posY = rand()%height;
+            posX = rand()%width;
+        }
+        board[rand()%height][rand()%width] = -2;
+    }
+    int poss[8][2] = {{0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 2}, {2, 0}, {2, 1}, {2, 2}};
+    for(int i = 0; i<8; i++){
+        board[poss[i][0]][poss[i][1]] = -1;
+    }
     clear_board(board);
     refresh();
     move(pos[0], pos[1]);
